86f5e10032803c52385ed84e11ed17ce67633898
Mudit Jain <mudjain18@gmail.com>
2016-03-14 18:42:39 +0530

    Uploading INFSDK

---
+++ InformSDK.framework/Headers/InformSDK.h
@@ -0,0 +1,19 @@
+//
+//  InformSDK.h
+//  InformSDK
+//
+//  Created by Mudit on 09/12/15.
+//  Copyright © 2015 INFORM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InformSDK.
+FOUNDATION_EXPORT double InformSDKVersionNumber;
+
+//! Project version string for InformSDK.
+FOUNDATION_EXPORT const unsigned char InformSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InformSDK/PublicHeader.h>
+
+
